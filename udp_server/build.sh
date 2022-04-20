9d6ca6396629c319ef9ca5834dbd762ca67dd580
jingpingyin <jingpingyin@autox.ai>
2022-04-20 17:25:33 +0800

    Create md5 for bin.

--- udp_server/build.sh
+++ udp_server/build.sh
@@ -4,5 +4,7 @@ K8S_REPO="hub.aa.ai/infra/xremote"
 
 cargo build --release
 
+md5sum target/release/test-udp-server > sum.md5
 docker build -t "${K8S_REPO}/test-udp-server:1.0.0" -f k8s/Dockerfile .
 docker push "${K8S_REPO}/test-udp-server:1.0.0"
+rm sum.md5
